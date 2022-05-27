203820b33c24fc3fe2140eed135ff3a64e7674be
Kenny Kerr <kenny@kennykerr.ca>
2022-05-26 20:43:04 -0700

    tree

--- crates/tools/crates/src/main.rs
+++ crates/tools/crates/src/main.rs
@@ -4,28 +4,27 @@ use std::collections::*;
 fn main() {
     let files = vec![File::new("crates/libs/metadata/default/Windows.winmd").unwrap(), File::new("crates/libs/metadata/default/Windows.Win32.winmd").unwrap(), File::new("crates/libs/metadata/default/Windows.Win32.Interop.winmd").unwrap()];
     let reader = &Reader::new(&files);
-    let root = reader.tree("Windows").unwrap();
-    let mut crates = BTreeMap::<String, Vec<&str>>::new();
-    for tree in root.flatten() {
-        let split: Vec<&str> = tree.namespace.split('.').collect();
-        if split.get(1) == Some(&"Win32") {
-            if let Some(name) = split.get(2) {
-                crates.entry(format!("Win32.{}", name)).or_default().push(tree.namespace);
-            }
-        } else if let Some(name) = split.get(1) {
-            crates.entry(name.to_string()).or_default().push(tree.namespace);
-        }
-    }
     let _ = std::fs::remove_dir_all("crates/generated");
-    for (name, namespaces) in crates {
-        build(reader, &name, &namespaces, true);
-        build(reader, &name, &namespaces, false);
+    let root = reader.tree("Windows").unwrap();
+    for (name, tree) in root.nested {
+        if name == "Win32" {
+            for tree in tree.nested.values() {
+                build(reader, &tree);
+            }
+        } else {
+            build(reader, &tree);
+        }
     }
 }
 
-fn build(reader: &Reader, root: &str, namespaces: &[&str], sys: bool) {
+fn build(reader:&Reader, tree:&Tree) {
+    build_kind(reader, tree, true);
+    build_kind(reader, tree, false);
+}
+
+fn build_kind(reader: &Reader, tree: &Tree, sys: bool) {
     let name = if sys { "windows-sys" } else { "windows" };
-    let name = format!("{}-{}", name, root.to_lowercase().replace(".", "-"));
+    let name = format!("{}-{}", name, tree.namespace.strip_prefix("Windows.").unwrap().to_lowercase().replace(".", "-"));
     println!("{}", name);
     let mut path = std::path::PathBuf::from("crates/generated");
     path.push(&name);
