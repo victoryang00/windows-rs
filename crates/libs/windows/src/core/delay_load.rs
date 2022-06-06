9f4f023ccf89bb9388a6ed2235be96cc3b2cca73
Kenny Kerr <kenny@kennykerr.ca>
2022-06-06 14:00:30 -0700

    delay_load

--- crates/libs/windows/src/core/delay_load.rs
+++ crates/libs/windows/src/core/delay_load.rs
@@ -9,9 +9,11 @@ use windows_sys::Win32::System::LibraryLoader::*;
 ///
 /// * Both the library and function names must be valid PCSTR representations
 pub unsafe fn delay_load(library: &[u8], function: &[u8]) -> Result<*mut core::ffi::c_void> {
-    let library = LoadLibraryA(library.as_ptr())?;
-
-    if let Some(address) = GetProcAddress(library, PCSTR(function.as_ptr())) {
+    let library = LoadLibraryA(library.as_ptr());
+    if library == 0 {
+        return Err(Error::from_win32());
+    }
+    if let Some(address) = GetProcAddress(library, function.as_ptr()) {
         Ok(address as _)
     } else {
         FreeLibrary(library);
